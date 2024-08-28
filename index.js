01eb4080c3dee7cb971156e35876df71f9aed98b
HNORAYR <hnorik@mail.ru>
2024-08-28 14:43:17 +0400

    new commit

--- index.js
+++ index.js
@@ -4,24 +4,24 @@ const bodyParser = require('body-parser');
 const {MongoClient} = require("mongodb");
 const client = new MongoClient("mongodb://localhost:27017");
 client.connect();
-let db = client.db("user");
+let db = client.db("Norayr");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
 app.use(express.json())
-app.get('/', async(req, res) => {
+app.get('/products', async(req, res) => {
   const collection = db.collection("products");
   const results = await collection.find({}).toArray();
   res.json({products: results})
 });
-app.post('/submit', async(req, res) => {
+app.post('/products', async(req, res) => {
     const {name, price} = req.body;
     const collection = db.collection("products");
     await collection.insertOne({name: name, price: price});
     res.send(`Received data - Name: ${name}, Price ${price}`);
   })
-app.listen(3000,() => console.log("Server http://localhost:3000 is ready"));
+app.listen(3017,() => console.log("Server http://localhost:3017 is ready"));
 
 
